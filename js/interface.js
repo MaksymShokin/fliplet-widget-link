a1235a66530a5d749ec3dac15ae197b3d977e6ac
Nicholas Valbusa <squallstar@gmail.com>
2017-03-02 10:52:54 +0100

    Revert "first commit"
    This reverts commit 1a675952ba07904ac999ef0c91190cbb3a9ef70b.

--- js/interface.js
+++ js/interface.js
@@ -1,18 +1,102 @@
-// Read the input data
-var data = Fliplet.Widget.getData();
+var widgetInstanceId = $('[data-widget-id]').data('widget-id');
+var widgetInstanceData = Fliplet.Widget.getData(widgetInstanceId) || {};
+var validInputEventName = 'interface-validate';
 
-// Fired when the external save button is clicked
-Fliplet.Widget.onSaveRequest(function () {
+var fields = [
+  'linkLabel',
+  'action',
+  'page',
+  'document',
+  'transition',
+  'url',
+  'query'
+];
 
-  // Sample data retrieval
-  data.sample = $('input[name="sample"]').val();
-
-  // Save the result
-  Fliplet.Widget.save(data).then(function () {
-    // Tell the UI this widget has finished
-    Fliplet.Widget.complete();
-  });
+Fliplet.Widget.emit(validInputEventName, {
+  isValid: false
 });
 
-// Sample to read data sources
-Fliplet.DataSources.get().then(console.log);
+$('#action').on('change', function onLinkTypeChange() {
+  var selectedValue = $(this).val();
+  var selectedText = $(this).find("option:selected").text();
+  $('.section.show').removeClass('show');
+  $('#' + selectedValue + 'Section').addClass('show');
+  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
+
+  Fliplet.Widget.emit(validInputEventName, {
+    isValid: selectedValue !== 'none'
+  });
+
+  // Tells the parent widget this provider has changed its interface height
+  Fliplet.Widget.autosize();
+});
+
+$('#page').on('change', function onScreenListChange() {
+  var selectedText = $(this).find("option:selected").text();
+  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
+});
+
+$('#transition').on('change', function onTransitionListChange() {
+  var selectedText = $(this).find("option:selected").text();
+  $(this).parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
+});
+
+$('#add-query').on('click', function() {
+  $(this).addClass('hidden');
+  $(this).parents('#screen-form').addClass('show-query');
+});
+
+// Fired from Fliplet Studio when the external save button is clicked
+Fliplet.Widget.onSaveRequest(function () {
+  $('form').submit();
+});
+
+// Save data when submitting the form
+$('form').submit(function (event) {
+  event.preventDefault();
+
+  // Clean data to store the new saved values
+  var data = {};
+
+  // Attach options from widgetInstanceData
+  data.options = widgetInstanceData.options;
+
+  // Get and save values to data
+  fields.forEach(function (fieldId) {
+    data[fieldId] = $('#' + fieldId).val();
+  });
+
+  if (data.url && !data.url.match(/^[A-z]+:/i)) {
+    data.url = 'http://' + data.url;
+  }
+
+  // TODO: validate query
+
+  Fliplet.Widget.save(data).then(function () {
+    Fliplet.Widget.complete();
+  });
+
+});
+
+function initialiseData() {
+  if (widgetInstanceData.action) {
+    fields.forEach(function (fieldId) {
+      $('#' + fieldId).val(widgetInstanceData[fieldId]).change();
+    });
+  }
+}
+
+Fliplet.Pages.get()
+  .then(function (pages) {
+    var $select = $('#page');
+    (pages || []).forEach(function (page) {
+      $select.append(
+        '<option value="' + page.id + '"' +
+        (widgetInstanceData.page === page.id.toString() ? ' selected' : '') +
+        '>' + page.title + '</option>'
+      );
+    });
+
+    return Promise.resolve();
+  })
+  .then(initialiseData);
